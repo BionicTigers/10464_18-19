4e790e8324a2f16eb499741ca403c362cd4c2823
codingchris779 <33784082+codingchris779@users.noreply.github.com>
2018-08-26 20:41:17 -0400

    un disabled cruise deleted vex drivetrain

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PositionTracking.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PositionTracking.java
@@ -1,25 +1,25 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import org.firstinspires.ftc.teamcode.CruiseControlTest;
+import com.qualcomm.hardware.bosch.BNO055IMU;
 
-@TeleOp(name="Position", group ="summerProjects")
+@Autonomous(name="POS", group ="summerProjects")
 //This class is designed to do some testing with the boch IMU chip onboard the REV
 public class PositionTracking extends LinearOpMode {
-    public DcMotor motorFrontRight;
-    public DcMotor motorFrontLeft;
-    public DcMotor motorBackRight;
-    public DcMotor motorBackLeft;
     @Override
     public void runOpMode() throws InterruptedException {
         //Bellow is an overly complex way of going forward a bit :)
         int rotations = randomWithRange(3,7);
         CruiseControlTest test = new CruiseControlTest();
         test.setCruiseControl(50,rotations);
-
+        //Next we will try and use the imu chip to guess how far we went.
+        //**note** needs testing on venux or mercury not board
+        telemetry.addData("hi",rotations);
+        telemetry.update();
     }
    public int randomWithRange(int min, int max)
     {
