7911b5c7ee52d254045efd28b5064ffb045f80d8
codingchris779 <33784082+codingchris779@users.noreply.github.com>
2018-09-10 20:18:27 -0400

    HEY JoJo Please run this in robotics

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PositionTracking.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PositionTracking.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 import android.support.annotation.NonNull;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -13,6 +14,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 
 import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.*;
 
 
 import org.firstinspires.ftc.robotcore.external.navigation.MagneticFlux;
@@ -26,177 +28,38 @@ import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
 
 public class PositionTracking extends LinearOpMode
 {
-    private BNO055IMU imu;
-    private float hsvValues[] = {0.0f, 0.0f, 0.0f};
 
-    //
-    // Implements FtcOpMode abstract methods.
-    //
 
+
+
+
+    // State used for updating telemetry
+    Orientation angles;
+    Acceleration gravity;
 
     @Override public void runOpMode()
     {
-        //
-        // Initializing sensors on or connected to the REV hub.
-        //
+        BNO055IMU imu;
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.RADIANS;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+        parameters.loggingEnabled = true;
+        parameters.loggingTag = "IMU";
+        parameters.accelerationIntegrationAlgorithm = imu.startAccelerationIntegration(Position, Velocity, int);
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
 
-        imu = new BNO055IMU() {
-            @Override
-            public boolean initialize(@NonNull Parameters parameters) {
-                return false;
-            }
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.getPosition();
+        imu.initialize(parameters);
 
-            @NonNull
-            @Override
-            public Parameters getParameters() {
-                return null;
-            }
 
-            @Override
-            public void close() {
-
-            }
-
-            @Override
-            public Orientation getAngularOrientation() {
-                return null;
-            }
-
-            @Override
-            public Orientation getAngularOrientation(AxesReference reference, AxesOrder order, org.firstinspires.ftc.robotcore.external.navigation.AngleUnit angleUnit) {
-                return null;
-            }
-
-            @Override
-            public Acceleration getOverallAcceleration() {
-                return null;
-            }
-
-            @Override
-            public AngularVelocity getAngularVelocity() {
-                return null;
-            }
-
-            @Override
-            public Acceleration getLinearAcceleration() {
-                return null;
-            }
-
-            @Override
-            public Acceleration getGravity() {
-                return null;
-            }
-
-            @Override
-            public Temperature getTemperature() {
-                return null;
-            }
-
-            @Override
-            public MagneticFlux getMagneticFieldStrength() {
-                return null;
-            }
-
-            @Override
-            public Quaternion getQuaternionOrientation() {
-                return null;
-            }
-
-            @Override
-            public Position getPosition() {
-                return null;
-            }
-
-            @Override
-            public Velocity getVelocity() {
-                return null;
-            }
-
-            @Override
-            public Acceleration getAcceleration() {
-                return null;
-            }
-
-            @Override
-            public void startAccelerationIntegration(Position initialPosition, Velocity initialVelocity, int msPollInterval) {
-
-            }
-
-            @Override
-            public void stopAccelerationIntegration() {
-
-            }
-
-            @Override
-            public SystemStatus getSystemStatus() {
-                return null;
-            }
-
-            @Override
-            public SystemError getSystemError() {
-                return null;
-            }
-
-            @Override
-            public CalibrationStatus getCalibrationStatus() {
-                return null;
-            }
-
-            @Override
-            public boolean isSystemCalibrated() {
-                return false;
-            }
-
-            @Override
-            public boolean isGyroCalibrated() {
-                return false;
-            }
-
-            @Override
-            public boolean isAccelerometerCalibrated() {
-                return false;
-            }
-
-            @Override
-            public boolean isMagnetometerCalibrated() {
-                return false;
-            }
-
-            @Override
-            public CalibrationData readCalibrationData() {
-                return null;
-            }
-
-            @Override
-            public void writeCalibrationData(CalibrationData data) {
-
-            }
-
-            @Override
-            public byte read8(Register register) {
-                return 0;
-            }
-
-            @Override
-            public byte[] read(Register register, int cb) {
-                return new byte[0];
-            }
-
-            @Override
-            public void write8(Register register, int bVal) {
-
-            }
-
-            @Override
-            public void write(Register register, byte[] data) {
-
-            }
-        };
         while (true ){
             telemetry.addData("orientation:", "Angle:x=%6.1f,y=%6.1f,z=%6.1f",
-                    imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES).firstAngle,
-                    imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES).secondAngle,
-                    imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES).thirdAngle);
+                   imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES).firstAngle,
+                   imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES).secondAngle,
+                   imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES).thirdAngle);
             telemetry.addData("Velocity", "Vel: x=%6.1f,y=%6.1f,z=%6.1f",
                     imu.getVelocity().xVeloc,
                     imu.getVelocity().yVeloc,
